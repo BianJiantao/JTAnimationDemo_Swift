1863cd16ce518bef190dda334325848e047ad6af
Bjt <jiantaobian@qq.com>
2017-09-03 15:45:15 +0800

    项目初始化

---
+++ JTAnimations/JTAnimations/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  JTAnimations
+//
+//  Created by BJT on 2017/9/3.
+//  Copyright © 2017年 BJT. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+
+}
+
